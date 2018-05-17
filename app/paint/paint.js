3d948ba78300fb82ee263b538ae303c6265dbf36
Brandon DeWitt <34219808+brandondewitt89@users.noreply.github.com>
2018-05-17 16:24:44 -0700

    Update paint.js
    Added single cell coloring functionality and a clear palette function.
    Need to encapsulate and clean up code.

--- app/paint/paint.js
+++ app/paint/paint.js
@@ -9,10 +9,14 @@ const RENDER_DELAY_MILLISECONDS = 100;
 const COLOR_RED   = 0XFF0000;
 const COLOR_GREEN = 0X00FF00;
 const COLOR_BLUE  = 0X0000FF;
+const COLOR_BLACK = 0X000000;
 
 // initial conditions
 let fillColor = 0X3C2F2F;
 let paintColor = 0X3C2F2F;
+let testColor = 0X0000FF;
+
+let paintCells = [0, 0];
 
 
 const board = document.querySelector('#board');
@@ -32,37 +36,61 @@ document.getElementById("btnFillRed").onclick = function() {changeFillColor(COLO
 document.getElementById("btnFillGreen").onclick = function() {changeFillColor(COLOR_GREEN)};
 document.getElementById("btnFillBlue").onclick = function() {changeFillColor(COLOR_BLUE)};
 document.getElementById("btnFillPicker").onclick = function() {changeFillColor(paintColor)};
+document.getElementById("btnClear").onclick = function() {clearPaintCells(paintCells)};
 
 function changeFillColor(colorIn) {
     fillColor = colorIn;
 	// alert ("Color Change!");
 }
 
+const containsCell = (list, cellToCheck) => {
+  for (let cell of list) {
+    if (cellsEqual(cell, cellToCheck)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const cellsEqual = (a, b) => {
+  return a[0] === b[0] && a[1] === b[1];
+};
+
+function clearPaintCells(list) {
+  list.splice(0);
+}
 
 const render = () => {
 
-  pixelBuffer.fill(fillColor);
+  pixelBuffer.fill(COLOR_BLACK);
 
-  // for (let j = 0; j < BOARD_HEIGHT; j++) {
-  //   for (let i = 0; i < BOARD_WIDTH; i++) {
-  //
-  //     for (let snake of snakes) {
-  //       if (containsCell(snake.scoreCells, [i, j])) {
-  //         pixelBuffer[j*BOARD_WIDTH + i]  = snake.scoreColor;
-  //       }
-  //       if (snake.coversCell([i, j])) {
-  //         pixelBuffer[j*BOARD_WIDTH + i]  = snake.bodyColor;
-  //       }
-  //       else if (containsCell(apples, [i, j])) {
-  //         pixelBuffer[j*BOARD_WIDTH + i]  = appleColor;
-  //       }
-  //     }
-  //   }
-  // }
+  for (let j = 0; j < BOARD_HEIGHT; j++) {
+    for (let i = 0; i < BOARD_WIDTH; i++) {
+		if (containsCell(paintCells, [i, j])) {
+          pixelBuffer[j*BOARD_WIDTH + i] = fillColor;
+        }
+    }
+  }
 
   browserDisplay.render(pixelBuffer);
   display.render(pixelBuffer);
 };
+
+function printMousePos(event) {
+	let relX = event.offsetX;
+	let relY = event.offsetY;
+	let PixelX = Math.trunc(relX / 20);
+	let PixelY = Math.trunc(relY / 20);
+	
+	const paintCell = [PixelX, PixelY]
+	paintCells.push(paintCell);
+
+
+	// console.log("offsetX: " + relX + " - offsetY: " + relY);
+	// console.log("PixelX: " + PixelX + " - PixelY: " + PixelY);
+	}
+
+browserDisplay._canvas.addEventListener("click", printMousePos);
 
 $(document).ready(function () {
   $(".pick-a-color").pickAColor({
