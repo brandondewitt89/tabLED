aad1047e2ad2db12b992d879dc33a6ea8e54cc9c
Brandon DeWitt <34219808+brandondewitt89@users.noreply.github.com>
2018-05-24 06:38:01 -0700

    Add files via upload

---
+++ app/js/index.js
@@ -0,0 +1,16 @@
+//
+// document.getElementById("btnHome").onclick = function() {toHome()};
+// document.getElementById("btnPaint").onclick = function() {toPaint()};
+// document.getElementById("btnSnake").onclick = function() {toSnake()};
+//
+// function toHome() {
+// 	window.open("file:///Users/brandondewitt/Downloads/tabLED-master/app/index.html","_self")
+// }
+//
+// function toPaint() {
+// 	window.open("file:///Users/brandondewitt/Downloads/tabLED-master/app/paint.html","_self")
+// }
+//
+// function toSnake() {
+// 	window.open("file:///Users/brandondewitt/Downloads/tabLED-master/app/snake.html","_self")
+// }
