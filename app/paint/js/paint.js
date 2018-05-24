a6b3c75d32a837a6296f08507028d693daa04c23
Brandon DeWitt <34219808+brandondewitt89@users.noreply.github.com>
2018-05-23 17:08:26 -0700

    Update and rename app/paint/paint.js to app/paint/js/paint.js
    changed file location.
    added mode buttons.

--- app/paint/js/paint.js
+++ app/paint/js/paint.js
@@ -52,6 +52,16 @@ document.getElementById("btnSaveImage").onclick = function() {saveImage(pixels)}
 document.getElementById("btnLoadImage").onclick = function() {loadImage()};
 document.getElementById("btnFillPicker").onclick = function() {changeFillColor(color)};
 document.getElementById("btnClear").onclick = function() {changeFillColor(COLOR_BLACK)};
+document.getElementById("btnHome").onclick = function() {toHome()};
+document.getElementById("btnSnake").onclick = function() {toSnake()};
+
+function toHome() {
+	window.open("file:///C:/Users/brandon.dewitt/Downloads/tabLED-master/app/index.html","_self")
+}
+
+function toSnake() {
+	window.open("file:///C:/Users/brandon.dewitt/Downloads/tabLED-master/app/snake.html","_self")
+}
 
 function saveImage(temp) {
   var image = new Array(BOARD_WIDTH);
